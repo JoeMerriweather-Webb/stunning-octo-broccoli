1d00d2c2d18397f067b1411564ba3287a0680418
Joe Merriweather-Webb <4924691+JoeMerriweather-Webb@users.noreply.github.com>
2024-08-31 15:35:35 -0700

    Created Phoenix app with phx.new

--- app/config/test.exs
+++ app/config/test.exs
@@ -1,10 +1,5 @@
 import Config
 
-# Configure your database
-#
-# The MIX_TEST_PARTITION environment variable can be used
-# to provide built-in test partitioning in CI environment.
-# Run `mix help test` for more information.
 config :app, App.Repo,
   username: "postgres",
   password: "postgres",
@@ -13,19 +8,10 @@ config :app, App.Repo,
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: System.schedulers_online() * 2
 
-# We don't run a server during test. If one is required,
-# you can enable the server option below.
 config :app, AppWeb.Endpoint,
   http: [ip: {127, 0, 0, 1}, port: 4002],
-  secret_key_base: "QJNZfJAD484iAvMP4rN2pmY3MKE5Yv8W4D4wQTH48SMaGbZRfpLx/1CeNQxfmvWk",
+  secret_key_base: "zAVViFVJDNauYk7bvAkruxr2T3aCq/Qn66x330qbMqJnapvVrtrqoyGz04ZKLiWV",
   server: false
 
-# Print only warnings and errors during test
 config :logger, level: :warning
-
-# Initialize plugs at runtime for faster test compilation
 config :phoenix, :plug_init_mode, :runtime
-
-# Enable helpful, but potentially expensive runtime checks
-config :phoenix_live_view,
-  enable_expensive_runtime_checks: true
